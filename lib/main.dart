4c9ca999576824693b380bcbbcd845ce3c4962ec
tan8325 <tbach4@student.gsu.edu>
2025-02-12 13:45:06 -0500

    Added theme, input, and animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,119 +8,178 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Heartbeat Animation',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HeartBeatScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HeartBeatScreen extends StatefulWidget {
+  const HeartBeatScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HeartBeatScreenState createState() => _HeartBeatScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HeartBeatScreenState extends State<HeartBeatScreen>
+    with SingleTickerProviderStateMixin {
+  bool _isExpanded = false;
+  int _seconds = 30;
+  Timer? _timer;
+  bool _animationStarted = false;
+  String _partnerName = "";
+  final TextEditingController _nameController = TextEditingController();
 
-  void _incrementCounter() {
+  final List<String> _messages = [
+    "Love is in the air!", "Happy Valentine's Day!", "You make my heart race!",
+    "Forever and always ❤️", "You are my heartbeat!"
+  ];
+  int _currentMessageIndex = 0;
+
+  void _startAnimation() {
+    if (_partnerName.isEmpty) return;
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _animationStarted = true;
+      _seconds = 30;
+    });
+
+    _startHeartbeat();
+    _startTimer();
+  }
+
+  void _startHeartbeat() {
+    Timer.periodic(const Duration(milliseconds: 800), (timer) {
+      if (!mounted) {
+        timer.cancel();
+      } else {
+        setState(() {
+          _isExpanded = !_isExpanded;
+        });
+      }
+    });
+  }
+
+  void _startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (!mounted || _seconds <= 0) {
+        timer.cancel();
+      } else {
+        setState(() {
+          _seconds--;
+          _currentMessageIndex = (_currentMessageIndex + 1) % _messages.length;
+        });
+      }
     });
   }
 
   @override
+  void dispose() {
+    _timer?.cancel();
+    _nameController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Colors.pink[50],
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (_animationStarted) ...[
+                  AnimatedContainer(
+                    duration: const Duration(milliseconds: 800),
+                    width: _isExpanded ? 200 : 170,
+                    height: _isExpanded ? 200 : 170,
+                    child: Image.asset("assets/images/heart.png"),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    "Time left: $_seconds s",
+                    style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold, color: Colors.pink,
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  Text(
+                    "${_messages[_currentMessageIndex]} ${_partnerName.isNotEmpty ? _partnerName : ''}",
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red,
+                    ),
+                  ),
+                ],
+                if (!_animationStarted) _buildStartBox(),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+          _buildFloatingHearts(),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _buildStartBox() {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(
+            width: 300,
+            child: TextField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                hintText: "Enter your partner's name",
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _partnerName = value;
+                });
+              },
+            ),
+          ),
+          const SizedBox(height: 20),
+          IconButton(
+            icon: const Icon(Icons.play_circle_fill, size: 60, color: Colors.pink),
+            onPressed: _startAnimation,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildFloatingHearts() {
+    return Stack(
+      children: List.generate(10, (index) {
+        return AnimatedPositioned(
+          duration: Duration(seconds: 5 + index),
+          left: (index * 40).toDouble(),
+          bottom: _isExpanded ? 0 : -100,
+          child: Icon(
+            Icons.favorite,
+            color: Colors.pink.withOpacity(0.6),
+            size: 30 + (index % 3 * 10),
+          ),
+        );
+      }),
     );
   }
 }
